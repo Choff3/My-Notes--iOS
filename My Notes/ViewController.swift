4199706dc577ece00ea988a478249dd82b184eed
Charles Hoff <choff3@student.gsu.edu>
2019-04-23 10:13:33 -0700

    Initial Commit

---
+++ My Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Notes
+//
+//  Created by Teddy Hoff on 4/23/19.
+//  Copyright © 2019 TBA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
